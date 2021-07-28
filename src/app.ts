3a17be4011f90190f92cd1dfdc501bb0209bec4a
João Victor <jvictorgmodesto@gmail.com>
2021-07-27 23:52:53 -0300

    configuracao docker e mongoDB connection

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import express from "express";
 import { PokemonController } from "./pokemon.controller";
 import { PokemonService } from "./services/pokemon.service";
+import mongoose from "mongoose";
+import { MONGO_URL } from "./constants/pokeApi.constants";
 
 class App {
   public app: Application;
@@ -11,15 +13,13 @@ class App {
   constructor() {
     this.app = express();
     this.setConfig();
+    this.setMongoConfig();
     this.setControllers();
   }
 
   private setConfig() {
-    // Allows us to receive requests with data in json format
     this.app.use(bodyParser.json({ limit: "50mb" }));
-    // Allows us to receive requests with data in x-www-form-urlencoded format
     this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-    // Enables cors
     this.app.use(cors());
   }
 
@@ -29,6 +29,14 @@ class App {
 
     // Telling express to use our Controller's routes
     this.app.use("/pokemon", pokemonController.router);
+  }
+
+  private setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+    });
   }
 }
 
--- src/constants/pokeApi.constants.ts
+++ src/constants/pokeApi.constants.ts
@@ -1,3 +1,4 @@
 //src/constants/pokeApi.constants.ts
 export const PORT = 9001;
 export const WELCOME_MESSAGE = "Welcome to PokeAPI";
+export const MONGO_URL = "mongodb://localhost:27017/Pokemon";

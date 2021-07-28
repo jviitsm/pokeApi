ee2f233a3d96485dc6bd1fc14d07be3eb7114b19
João Victor <jvictorgmodesto@gmail.com>
2021-07-28 01:51:53 -0300

    model e delete criados

--- src/pokemon.controller.ts
+++ src/pokemon.controller.ts
@@ -1,20 +1,51 @@
-import { Request, Response, Router } from "express";
+import { request, Request, Response, Router } from "express";
 import { PokemonService } from "./services/pokemon.service";
 
-export class PokemonController{
-    public router = Router();
+export class PokemonController {
+  public router = Router();
 
-    constructor(private pokemonService: PokemonService){
-        this.setRoutes();
+  constructor(private pokemonService: PokemonService) {
+    this.setRoutes();
+  }
+
+  public setRoutes() {
+    this.router.get("/", this.sayHello);
+    this.router.post("/", this.add);
+    this.router.get("/all", this.findAll);
+    this.router.route("/:id").delete(this.delete);
+  }
+
+  private sayHello = (_: Request, res: Response) => {
+    const welcomeMessage = this.pokemonService.getWelcomeMessage();
+    res.send(welcomeMessage);
+  };
+
+  private findAll = async (_: Request, res: Response) => {
+    try {
+      const pokemon = await this.pokemonService.findAll();
+      res.send(pokemon);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
+  };
 
-    public setRoutes(){
-        this.router.get("/", this.sayHello);
+  private add = async (_: Request, res: Response) => {
+    try {
+      const addPokemonResult = await this.pokemonService.add(_.body);
+      res.send(addPokemonResult);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
+  };
 
-    private sayHello = (_: Request, res: Response) => {
-        const welcomeMessage = this.pokemonService.getWelcomeMessage();
-        res.send(welcomeMessage);
+  private delete = async (req: Request, res: Response) => {
+    try {
+      const deletedPokemonResult = await this.pokemonService.delete(
+        req.params.id
+      );
+      res.send(deletedPokemonResult);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
-
+  };
 }
--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,6 +1,30 @@
+import { Ipokemon } from "../interfarces/pokemon.interface";
+import { Pokemon } from "../model/pokemon.model";
 import { WELCOME_MESSAGE } from "../constants/pokeApi.constants";
+
 export class PokemonService {
-  public getWelcomeMessage() {
+  public getWelcomeMessage(): string {
     return WELCOME_MESSAGE;
+  }
+
+  public findAll(): Promise<Ipokemon[]> {
+    return Pokemon.find({}).exec();
+  }
+
+  public add(pokemon: Ipokemon): Promise<Ipokemon> {
+    const newPokemon = new Pokemon(pokemon);
+    return newPokemon.save();
+  }
+
+  public async delete(id: string) {
+    const deletedPokemon = await Pokemon.findByIdAndDelete(
+      id
+    ).exec();
+
+    if (!deletedPokemon) {
+      throw new Error(`Pokemon with id '${id}' not found`);
+    }
+
+    return deletedPokemon;
   }
 }
